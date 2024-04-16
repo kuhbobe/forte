2b4e0569791e4734ce85e21d7842650fdad1502e
Kobe A <80867186+kuhbobe@users.noreply.github.com>
2024-04-16 13:08:49 -0700

    re-push: name change

--- music-player/src/App.js
+++ music-player/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test for Forte React App
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Cool things on this
         </a>
       </header>
     </div>
