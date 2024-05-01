5e2efbb4e356b62709926aa849cb0639f96481a6
Kobe A <andayakobe@gmail.com>
2024-05-01 13:38:34 -0700

    changed login page to match forte

--- music-player/src/screens/login.js
+++ music-player/src/screens/login.js
@@ -5,7 +5,7 @@ import './login.css'
 export default function Login() {
   return (
     <div className="login-page">
-      <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png" 
+      <img src="forte-transparent2.png" 
       alt="logo-spotify" 
       className = "logo"
     />
