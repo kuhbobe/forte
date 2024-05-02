a66722a372bab34a222d677e498eaec734c3a515
Kobe A <andayakobe@gmail.com>
2024-05-01 17:35:19 -0700

    fixed undefined markings in player

--- music-player/src/components/songCard/albumImage.js
+++ music-player/src/components/songCard/albumImage.js
@@ -4,7 +4,7 @@ import "./albumImage.css";
 export default function AlbumImage({ url }) {
   return (
     <div className="albumImage flex">
-      <img src={url} alt="album art" className="albumImage-art" />
+      <img src={url} alt=" " className="albumImage-art" />
       <div className="albumImage-shadow">
         <img src={url} alt="shadow" className="albumImage-shadow" />
       </div>
--- music-player/src/components/songCard/albumInfo.js
+++ music-player/src/components/songCard/albumInfo.js
@@ -11,16 +11,17 @@ export default function AlbumInfo({ album }) {
     <div className="albumInfo-card">
       <div className="albumName-container">
         <div className="marquee">
-          <p>{album?.name + " - " + artists?.join(", ")}</p>
+          <p>{album?.name && artists?.length > 0 ? `${album.name} - ${artists.join(", ")}` : ''}</p>
         </div>
       </div>
       <div className="album-info">
-        <p>{`${album?.name} is an ${album?.album_type} by ${artists?.join(
-          ", "
-        )} with ${album?.total_tracks} track(s)`}</p>
+        <p>{album?.name && artists?.length > 0 ? `${album.name} is an ${album.album_type} by ${artists.join(", ")} with ${album.total_tracks} track(s)` : ''}</p>
       </div>
       <div className="album-release">
-        <p>Release Date: {album?.release_date}</p>
+        <p>
+          Release Date: {album?.release_date ? album.release_date : ''}
+          {album?.release_date ? null : <span className="alt-text">(No release date available)</span>}
+        </p>
       </div>
     </div>
   );
