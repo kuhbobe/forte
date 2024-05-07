75ca7b12907809d9b7e10142de6eb28b1f48e11a
mganar <mganar@csu.fullerton.edu>
2024-05-07 11:35:59 -0700

    Update spotify.js
    Url to Uri

--- music-player/src/spotify.js
+++ music-player/src/spotify.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const authEndpoint = "https://accounts.spotify.com/authorize?"
 const clientID = "7708fc22a830427cb3c94009446b8366"
-const redirectUrl= "http://localhost:3000"
+const redirectUri= "http://localhost:3000"
 const scopes = ["user-library-read", "playlist-read-private"]
 
-export const loginEndpoint = `${authEndpoint}client_id=${clientID}&redirect_uri=${redirectUrl}&scope=${scopes.join(
+export const loginEndpoint = `${authEndpoint}client_id=${clientID}&redirect_uri=${redirectUri}&scope=${scopes.join(
     "%20"
 )}&response_type=token&show_dialog=true`;
 
